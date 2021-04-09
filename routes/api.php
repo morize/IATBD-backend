73474277d0af3b46f51af64c18c09603eee9dd45
Mauriccio <mauricemr@outlook.com>
2021-04-09 08:51:11 +0200

    implemented s

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/show", '\App\Http\Controllers\PetController@show')->middleware('auth:sanctum');
+
+Route::post("/account/login", [AuthenticatedSessionController::class, 'store']);
+Route::post("/account/register", [RegisteredUserController::class, 'store']);
+Route::post("/account/logout", [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
