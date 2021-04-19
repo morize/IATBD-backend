d0616b21828d6301c7008952cb17181d68f2fb15
Mauriccio <mauricemr@outlook.com>
2021-04-19 22:04:22 +0200

    checkpoint before installing breeze again

---
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class EmailVerificationNotificationController extends Controller
+{
+    /**
+     * Send a new email verification notification.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        Log::info($request);
+        
+        if ($request->user()->hasVerifiedEmail()) {
+            return "verified";
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return "sent";
+    }
+}
